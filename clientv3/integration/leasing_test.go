e0868f47e9eb0c7a7d8a1313888df8732395d4b5
visheshnp <visheshnp@gmail.com>
2017-05-19 18:18:43 -0700

    more cleanup

--- clientv3/integration/leasing_test.go
+++ clientv3/integration/leasing_test.go
@@ -24,32 +24,25 @@ import (
 	"github.com/coreos/etcd/pkg/testutil"
 )
 
-func TestLeasingGet(t *testing.T) {
+func TestLeasingPutGet(t *testing.T) {
 	defer testutil.AfterTest(t)
 
-	clus := integration.NewClusterV3(t, &integration.ClusterConfig{Size: 2})
+	clus := integration.NewClusterV3(t, &integration.ClusterConfig{Size: 3})
 	defer clus.Terminate(t)
 
 	c1 := clus.Client(0)
 	c2 := clus.Client(1)
-	//	c3 := clus.Client(2)
+	c3 := clus.Client(2)
 	lKV1, err := leasing.NewleasingKV(c1, "foo/")
 	lKV2, err := leasing.NewleasingKV(c2, "foo/")
-	//	lKV3, err := leasing.NewleasingKV(c3, "foo/")
+	lKV3, err := leasing.NewleasingKV(c3, "foo/")
 
 	resp1, err := lKV1.Get(context.TODO(), "abc")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	//clus.Members[0].InjectPartition(t, clus.Members[1:])
-
 	if _, err := lKV1.Put(context.TODO(), "abc", "def"); err != nil {
-		t.Fatal(err)
-	}
-
-	resp1, err = lKV1.Get(context.TODO(), "abc")
-	if err != nil {
 		t.Fatal(err)
 	}
 
@@ -61,49 +54,35 @@ func TestLeasingGet(t *testing.T) {
 	if _, err := lKV2.Put(context.TODO(), "abc", "ghi"); err != nil {
 		t.Fatal(err)
 	}
-	/*
-		if _, err := lKV2.Put(context.TODO(), "abc", "jkl"); err != nil {
-			t.Fatal(err)
-		}
 
-		resp1, err = lKV1.Get(context.TODO(), "abc")
-		if err != nil {
-			t.Fatal(err)
-		}
-	*/
-	resp2, err = lKV2.Get(context.TODO(), "abc")
+	resp3, err := lKV3.Get(context.TODO(), "abc")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if string(resp1.Kvs[0].Key) != "abc" {
-		t.Errorf("expected key=%q, got key=%q", "abc", resp1.Kvs[0].Key)
-	}
-
-	if string(resp1.Kvs[0].Value) != "jkl" {
-		t.Errorf("expected value=%q, got value=%q", "bar", resp1.Kvs[0].Value)
+	if len(resp1.Kvs) != 0 {
+		t.Errorf("expected value=%q, got value=%q", "0", len(resp1.Kvs))
 	}
 
 	if string(resp2.Kvs[0].Key) != "abc" {
 		t.Errorf("expected key=%q, got key=%q", "abc", resp2.Kvs[0].Key)
 	}
 
-	if string(resp2.Kvs[0].Value) != "jkl" {
+	if string(resp2.Kvs[0].Value) != "def" {
 		t.Errorf("expected value=%q, got value=%q", "bar", resp2.Kvs[0].Value)
 	}
 
-	/*
-		if string(resp3.Kvs[0].Key) != "abc" {
-			t.Errorf("expected key=%q, got key=%q", "abc", resp3.Kvs[0].Key)
-		}
+	if string(resp3.Kvs[0].Key) != "abc" {
+		t.Errorf("expected key=%q, got key=%q", "abc", resp3.Kvs[0].Key)
+	}
 
-		if string(resp3.Kvs[0].Value) != "ghi" {
-			t.Errorf("expected value=%q, got value=%q", "bar", resp3.Kvs[0].Value)
-		}
-	*/
+	if string(resp3.Kvs[0].Value) != "ghi" {
+		t.Errorf("expected value=%q, got value=%q", "bar", resp3.Kvs[0].Value)
+	}
+
 }
 
-/*func TestLeasingGet2(t *testing.T) {
+func TestLeasingPutGet2(t *testing.T) {
 	defer testutil.AfterTest(t)
 
 	clus := integration.NewClusterV3(t, &integration.ClusterConfig{Size: 1})
@@ -117,7 +96,6 @@ func TestLeasingGet(t *testing.T) {
 		t.Fatal(err)
 	}
 }
-*/
 
 // TestLeasingInterval checks the leasing KV fetches key intervals.
 func TestLeasingInterval(t *testing.T) {
